5dive-ai/5dive-chat

feat(desktop): hyperlink selected composer text on link paste (#6684)
## Summary

Pasting a link while composer text is selected now turns that text into
a hyperlink instead of replacing it — the behavior people expect from
every other editor.

- `resolveExactLinkPaste` recognizes a clipboard payload that is exactly
one supported link (bare or `<…>`-wrapped `http(s)`, or a Buzz deep
link, canonicalized through the existing composer-link attribute
resolver) and returns the href to apply.
- The paste handler applies that href as a `link` mark over the current
selection, collapses the caret to the end of the newly linked range,
clears stored marks so subsequent typing isn't linked, and refocuses the
editor.

Edge cases handled:

- **No link mark available / selection can't carry marks** — falls
through to the existing replace-with-node path rather than silently
dropping the paste.
- **Selection contains a composer message-link chip** — keeps the old
replacement behavior instead of nesting a link over a chip.
- **Whitespace in the clipboard payload** — `unwrapExactHttpLink` now
rejects any text containing whitespace, so multi-line or "text plus URL"
pastes are not treated as an exact link.
- **Empty selection (caret paste)** — unchanged.
- **Idempotent re-paste** of an already-wrapped link is consumed rather
than doubling up.

## Testing

`desktop/src/features/messages/lib/composerMessageLinkNode.test.mjs`
gains coverage for exact-link resolution (per-shape accept/reject cases,
Buzz canonicalization) and for the paste handler: linking selected text,
canonicalizing Buzz links over a selection, idempotent wrapped-link
paste, fall-through when the selection can't carry marks, all-selection
collapse, chip-containing selections, and preserved caret-paste
behavior.

Full pre-push gate ran green on this branch: `file-size-check`,
`desktop-check`, `desktop-typecheck`, and `desktop-test` (5425 tests, 0
failures).


## Demo

Pasting an `https://` URL over selected composer text t

desktop/src/features/messages/lib/composerMessageLinkNode.ts
@@ -1,7 +1,8 @@
 import { mergeAttributes, Node } from "@tiptap/core";
-import type { Node as ProseMirrorNode } from "@tiptap/pm/model";
-import { TextSelection } from "@tiptap/pm/state";
+import type { Mark, Node as ProseMirrorNode } from "@tiptap/pm/model";
+import { Selection, TextSelection } from "@tiptap/pm/state";
 import type { EditorView } from "@tiptap/pm/view";
+import { find as findLinks } from "linkifyjs";
 
 import {
   buildIssueLink,
@@ -117,8 +118,116 @@ function unwrapExactBuzzLink(text: string): string | null {
 }
 
 function unwrapExactHttpLink(text: string): string | null {
+  if (!text || /\s/.test(text)) return null;
   const match = /^(?:<(https?:\/\/[^\s<>]+)>|(https?:\/\/\S+))$/i.exec(text);
   return match?.[1] ?? match?.[2] ?? null;
+}
+
+/**
+ * Resolves a clipboard payload that is exactly a supported link into the href
+ * the composer should apply when linkifying selected text on paste.
+ */
+export function resolveExactLinkPaste(
+  text: string,
+  resolveChannelName: ComposerMessageLinkNodeOptions["resolveChannelName"],
+): { href: string } | null {
+  const buzzHref = unwrapExactBuzzLink(text);
+  if (buzzHref) {
+    const attrs = resolveComposerMessageLinkAttributes(
+      buzzHref,
+      resolveChannelName,
+    );
+    return attrs ? { href: attrs.href } : null;
+  }
+
+  const httpHref = unwrapExactHttpLink(text);
+  return httpHref ? { href: httpHref } : null;
+}
+
+/**
+ * Resolves a clipboard payload for the *selected text* branch of paste
+ * handling, where this handler is the only one that runs.
+ *
+ * The exact Buzz/http matchers win first, so Buzz links keep their canonical
+ * form. Anything else falls back to linkify with `defaultProtocol: "http"` —
+ * the same matcher TipTap's `linkOnPaste` used before the composer took sole
+ * ownership of this branch, so `www.example.com`, `foo@example.com` and
+ * `ftp://…` still hyperlink the selection instead of replacing it.
+ *
+ * Deliberately scoped to the selection branch: broadening
+ * `resolveExactLinkPaste` would also change caret paste, where these shapes
+ * must keep arriving as plain text for `autolink` to pick up.
+ */
+export function resolveSelectionLinkPaste(
+  text: string,
+  resolveChannelName: ComposerMessageLinkNodeOptions["resolveChannelName"],
+): { href: string } | null {
+  const exactLinkPaste = resolveExactLinkPaste(text, resolveChannelName);
+  if (exactLinkPaste) return exactLinkPaste;
+
+  const link = findLinks(text, { defaultProtocol: "http" }).find(
+    (candidate) => candidate.isLink && candidate.value === text,
+  );
+  return link ? { href: link.href } : null;
+}
+
+function selectionContainsComposerMessageLinkNode(view: EditorView): boolean {
+  const { from, to } = view.state.selection;
+  let containsMessageLink = false;
+  view.state.doc.nodesBetween(from, to, (node) => {
+    if (containsMessageLink) return false;
+    if (node.type.name === COMPOSER_MESSAGE_LINK_NODE_NAME) {
+      containsMessageLink = true;
+      return false;
+    }
+    return true;
+  });
+  return containsMessageLink;
+}
+
+/**
+ * Checks the *outcome* of an `addMark` rather than predicting it: every inline
+ * node in the range must have come out carrying `mark`. Predicting is what a
+ * parent-level `allowsMarkType` probe does, and it misses mark exclusion —
+ * `code`'s `excludes: "_"` makes `Mark.addToSet` silently drop a link, so a
+ * selection spanning plain text and an inline code span passes the prediction
+ * but only gets partially linked.
+ */
+function everyInlineNodeCarriesMark(
+  doc: ProseMirrorNode,
+  from: number,
+  to: number,
+  mark: Mark,
+): boolean {
+  let containsInlineContent = false;
+  let allInlineContentCarriesMark = true;
+  doc.nodesBetween(from, to, (node) => {
+    if (!node.isInline) return true;
+    containsInlineContent = true;
+    if (!mark.isInSet(node.marks)) allInlineContentCarriesMark = false;
+    return true;
+  });
+  return containsInlineContent && allInlineContentCarriesMark;
+}
+
+function applyLinkToSelection(view: EditorView, href: string): boolean {
+  const { from, to } = view.state.selection;
+  const linkMark = view.state.schema.marks.link;
+  if (!linkMark) return false;
+
+  const mark = linkMark.create({ href });
+  let transaction = view.state.tr.addMark(from, to, mark);
+  // Bail before dispatching, so the document and selection are untouched and
+  // the paste falls through to normal replacement.
+  if (!everyInlineNodeCarriesMark(transaction.doc, from, to, mark))
+    return false;
+
+  transaction = transaction.setSelection(
+    Selection.near(transaction.doc.resolve(transaction.mapping.map(to)), -1),
+  );
+  view.dispatch(transaction.setStoredMarks([]).scrollIntoView());
+  view.focus();
+  return true;
 }
 
 function replaceSelectionWithNode(view: EditorView, node: ProseMirrorNode) {
@@ -140,6 +249,21 @@ export function createComposerLinkPasteHandler(
 ) {
   return (view: EditorView, event: ClipboardEvent): boolean => {
     const text = event.clipboardData?.getData("text/plain") ?? "";
+    if (
+      !view.state.selection.empty &&
+      !selectionContainsComposerMessageLinkNode(view)
+    ) {
+      const selectionLinkPaste = resolveSelectionLinkPaste(
+        text,
+        resolveChannelName,
+      );
+      if (selectionLinkPaste) {
+        if (!applyLinkToSelection(view, selectionLinkPaste.href)) return false;
+        event.preventDefault();
+        return true;
+      }
+    }
+
     const buzzHref = unwrapExactBuzzLink(text);
     const buzzLinkType =
       view.state.schema.nodes[COMPOSER_MESSAGE_LINK_NODE_NAME];

AI-Native-Ventures/Colony

(cherry picked from commit e8172b5baccd0f4a711f5e19a3bc4313708f1c78)
Signed-off-by: Basheer Phiri <phiribash@gmail.com>

desktop/src/features/messages/lib/composerMessageLinkNode.ts
@@ -1,7 +1,8 @@
 import { mergeAttributes, Node } from "@tiptap/core";
-import type { Node as ProseMirrorNode } from "@tiptap/pm/model";
-import { TextSelection } from "@tiptap/pm/state";
+import type { Mark, Node as ProseMirrorNode } from "@tiptap/pm/model";
+import { Selection, TextSelection } from "@tiptap/pm/state";
 import type { EditorView } from "@tiptap/pm/view";
+import { find as findLinks } from "linkifyjs";
 
 import { MENTION_CHIP_BASE_CLASSES } from "@/shared/ui/mentionChip";
 import {
@@ -60,8 +61,116 @@ function unwrapExactMessageLink(text: string): string | null {
 }
 
 function unwrapExactHttpLink(text: string): string | null {
+  if (!text || /\s/.test(text)) return null;
   const match = /^(?:<(https?:\/\/[^\s<>]+)>|(https?:\/\/\S+))$/i.exec(text);
   return match?.[1] ?? match?.[2] ?? null;
+}
+
+/**
+ * Resolves a clipboard payload that is exactly a supported link into the href
+ * the composer should apply when linkifying selected text on paste.
+ */
+export function resolveExactLinkPaste(
+  text: string,
+  resolveChannelName: ComposerMessageLinkNodeOptions["resolveChannelName"],
+): { href: string } | null {
+  const messageHref = unwrapExactMessageLink(text);
+  if (messageHref) {
+    const attrs = resolveComposerMessageLinkAttributes(
+      messageHref,
+      resolveChannelName,
+    );
+    return attrs ? { href: attrs.href } : null;
+  }
+
+  const httpHref = unwrapExactHttpLink(text);
+  return httpHref ? { href: httpHref } : null;
+}
+
+/**
+ * Resolves a clipboard payload for the *selected text* branch of paste
+ * handling, where this handler is the only one that runs.
+ *
+ * The exact Buzz/http matchers win first, so Buzz links keep their canonical
+ * form. Anything else falls back to linkify with `defaultProtocol: "http"` —
+ * the same matcher TipTap's `linkOnPaste` used before the composer took sole
+ * ownership of this branch, so `www.example.com`, `foo@example.com` and
+ * `ftp://…` still hyperlink the selection instead of replacing it.
+ *
+ * Deliberately scoped to the selection branch: broadening
+ * `resolveExactLinkPaste` would also change caret paste, where these shapes
+ * must keep arriving as plain text for `autolink` to pick up.
+ */
+export function resolveSelectionLinkPaste(
+  text: string,
+  resolveChannelName: ComposerMessageLinkNodeOptions["resolveChannelName"],
+): { href: string } | null {
+  const exactLinkPaste = resolveExactLinkPaste(text, resolveChannelName);
+  if (exactLinkPaste) return exactLinkPaste;
+
+  const link = findLinks(text, { defaultProtocol: "http" }).find(
+    (candidate) => candidate.isLink && candidate.value === text,
+  );
+  return link ? { href: link.href } : null;
+}
+
+function selectionContainsComposerMessageLinkNode(view: EditorView): boolean {
+  const { from, to } = view.state.selection;
+  let containsMessageLink = false;
+  view.state.doc.nodesBetween(from, to, (node) => {
+    if (containsMessageLink) return false;
+    if (node.type.name === COMPOSER_MESSAGE_LINK_NODE_NAME) {
+      containsMessageLink = true;
+      return false;
+    }
+    return true;
+  });
+  return containsMessageLink;
+}
+
+/**
+ * Checks the *outcome* of an `addMark` rather than predicting it: every inline
+ * node in the range must have come out carrying `mark`. Predicting is what a
+ * parent-level `allowsMarkType` probe does, and it misses mark exclusion —
+ * `code`'s `excludes: "_"` makes `Mark.addToSet` silently drop a link, so a
+ * selection spanning plain text and an inline code span passes the prediction
+ * but only gets partially linked.
+ */
+function everyInlineNodeCarriesMark(
+  doc: ProseMirrorNode,
+  from: number,
+  to: number,
+  mark: Mark,
+): boolean {
+  let containsInlineContent = false;
+  let allInlineContentCarriesMark = true;
+  doc.nodesBetween(from, to, (node) => {
+    if (!node.isInline) return true;
+    containsInlineContent = true;
+    if (!mark.isInSet(node.marks)) allInlineContentCarriesMark = false;
+    return true;
+  });
+  return containsInlineContent && allInlineContentCarriesMark;
+}
+
+function applyLinkToSelection(view: EditorView, href: string): boolean {
+  const { from, to } = view.state.selection;
+  const linkMark = view.state.schema.marks.link;
+  if (!linkMark) return false;
+
+  const mark = linkMark.create({ href });
+  let transaction = view.state.tr.addMark(from, to, mark);
+  // Bail before dispatching, so the document and selection are untouched and
+  // the paste falls through to normal replacement.
+  if (!everyInlineNodeCarriesMark(transaction.doc, from, to, mark))
+    return false;
+
+  transaction = transaction.setSelection(
+    Selection.near(transaction.doc.resolve(transaction.mapping.map(to)), -1),
+  );
+  view.dispatch(transaction.setStoredMarks([]).scrollIntoView());
+  view.focus();
+  return true;
 }
 
 function replaceSelectionWithNode(view: EditorView, node: ProseMirrorNode) {
@@ -83,6 +192,21 @@ export function createComposerLinkPasteHandler(
 ) {
   return (view: EditorView, event: ClipboardEvent): boolean => {
     const text = event.clipboardData?.getData("text/plain") ?? "";
+    if (
+      !view.state.selection.empty &&
+      !selectionContainsComposerMessageLinkNode(view)
+    ) {
+      const selectionLinkPaste = resolveSelectionLinkPaste(
+        text,
+        resolveChannelName,
+      );
+      if (selectionLinkPaste) {
+        if (!applyLinkToSelection(view, selectionLinkPaste.href)) return false;
+        event.preventDefault();
+        return true;
+      }
+    }
+
     const messageHref = unwrapExactMessageLink(text);
     const messageLinkType =
       view.state.schema.nodes[COMPOSER_MESSAGE_LINK_NODE_NAME];