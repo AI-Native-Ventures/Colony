5dive-ai/5dive-chat

fix(desktop): sender names in notifications + macOS click-through routing (#6427)
> Opened by the agents Brain and Pinky on behalf of @wesbillman.

Fixes two desktop notification issues (requested in Buzz channel
`desktop-notification-improvements`):

## 1. Notifications now show who sent the message

Live DM and thread-reply notifications showed only "Direct message" /
"Reply in #channel", while home-feed mention toasts already carried the
sender's name. All message-notification copy is now centralized:

- **`formatMessageNotification`**
(`notifications/lib/notificationFormat.ts`) — canonical title/body for
all five sources (mention, approval, needs-action, DM, thread reply).
Sender-first titles with neutral fallbacks, never a raw pubkey:
  - DM: `Taylor` instead of `Direct message`
- Thread reply: `Taylor replied in #ship-room` instead of `Reply in
#ship-room`
- **`useNotificationSenderName`** — synchronous cache-only lookup
(react-query users-batch entry cache → persisted label cache); a cold
miss ships the fallback title immediately and warms the cache in the
background. No toast delay, no new network machinery.
- **`buildEventNotificationTarget` / `buildFeedItemNotificationTarget`**
(`notifications/lib/target.ts`) — the click-through target payload is
built in one place instead of three hand-rolled copies.

## 2. macOS notification clicks route to the target message (#3509)

On packaged builds, clicking a notification focused the app but never
navigated. Three independent gaps lined up behind one symptom:

- **Reveal hang**: notification navigation now starts before the
best-effort `unminimize → show → setFocus` chain, so a hung native
invoke cannot gate click-through; the existing 1.5s reveal timeout
remains as a secondary guard.
- **Lost emit**: the Rust delegate queues the activation target *before*
emitting `native-notification-activated`; a lost emit stranded the
target with nothing re-draining the queue. The macOS listener now also
drains on window `focus

desktop/src/features/notifications/lib/notificationFormat.test.mjs
@@ -1,0 +1,163 @@
+import assert from "node:assert/strict";
+import test from "node:test";
+
+import { formatMessageNotification } from "./notificationFormat.ts";
+import { senderNameFromSummary } from "./senderName.ts";
+
+test("DM title is the sender name when resolved", () => {
+  const { title, body } = formatMessageNotification({
+    source: "dm",
+    senderName: "Taylor",
+    channelName: "taylor-wes",
+    content: "hey there",
+  });
+
+  assert.equal(title, "Taylor");
+  assert.equal(body, "hey there");
+});
+
+test("DM title falls back to the channel name, then generic copy", () => {
+  assert.equal(
+    formatMessageNotification({
+      source: "dm",
+      senderName: null,
+      channelName: "taylor-wes",
+      content: "hi",
+    }).title,
+    "taylor-wes",
+  );
+  assert.equal(
+    formatMessageNotification({
+      source: "dm",
+      senderName: "  ",
+      channelName: "",
+      content: "hi",
+    }).title,
+    "Direct message",
+  );
+});
+
+test("DM body falls back when the message is blank", () => {
+  assert.equal(
+    formatMessageNotification({
+      source: "dm",
+      senderName: "Taylor",
+      channelName: null,
+      content: "   ",
+    }).body,
+    "New message",
+  );
+});
+
+test("thread reply leads with the sender when resolved", () => {
+  assert.equal(
+    formatMessageNotification({
+      source: "thread_reply",
+      senderName: "Taylor",
+      channelName: "ship-room",
+      content: "done!",
+    }).title,
+    "Taylor replied in #ship-room",
+  );
+});
+
+test("thread reply preserves legacy copy when the sender is unknown", () => {
+  assert.equal(
+    formatMessageNotification({
+      source: "thread_reply",
+      senderName: null,
+      channelName: "ship-room",
+      content: "done!",
+    }).title,
+    "Reply in #ship-room",
+  );
+  assert.deepEqual(
+    formatMessageNotification({
+      source: "thread_reply",
+      senderName: null,
+      channelName: null,
+      content: "",
+    }),
+    { title: "Reply", body: "New reply" },
+  );
+});
+
+test("mention titles match the home-feed conventions", () => {
+  assert.equal(
+    formatMessageNotification({
+      source: "mention",
+      senderName: "Taylor",
+      channelName: "ship-room",
+      content: "@wes look",
+    }).title,
+    "Taylor mentioned you in #ship-room",
+  );
+  assert.equal(
+    formatMessageNotification({
+      source: "mention",
+      senderName: null,
+      channelName: "ship-room",
+      content: "@wes look",
+    }).title,
+    "@Mention in #ship-room",
+  );
+});
+
+test("approval and needs-action titles match the home-feed conventions", () => {
+  assert.deepEqual(
+    formatMessageNotification({
+      source: "approval",
+      senderName: "Taylor",
+      channelName: "ops",
+      content: "",
+    }),
+    {
+      title: "Taylor requested approval in #ops",
+      body: "A workflow is waiting for your approval.",
+    },
+  );
+  assert.deepEqual(
+    formatMessageNotification({
+      source: "needs_action",
+      senderName: null,
+      channelName: null,
+      content: "",
+    }),
+    {
+      title: "Needs Action",
+      body: "Something in Buzz needs your attention.",
+    },
+  );
+});
+
+test("senderNameFromSummary prefers displayName, then NIP-05, never a pubkey", () => {
+  assert.equal(
+    senderNameFromSummary({
+      displayName: "Taylor",
+      avatarUrl: null,
+      nip05Handle: "taylor@buzz.example",
+      ownerPubkey: null,
+    }),
+    "Taylor",
+  );
+  assert.equal(
+    senderNameFromSummary({
+      displayName: "  ",
+      avatarUrl: null,
+      nip05Handle: "taylor@buzz.example",
+      ownerPubkey: null,
+    }),
+    "taylor@buzz.example",
+  );
+  assert.equal(
+    senderNameFromSummary({
+      displayName: null,
+      avatarUrl: null,
+      nip05Handle: null,
+      ownerPubkey: null,
+    }),
+    null,
+  );
+  assert.equal(senderNameFromSummary(null), null);
+  assert.equal(senderNameFromSummary(undefined), null);
+});

desktop/src/features/notifications/lib/notificationFormat.ts
@@ -47,3 +47,66 @@ export function formatNotificationTitle(opts: {
     ? `${opts.prefix} in ${opts.channelLabel}`
     : opts.prefix;
 }
+
+export type MessageNotificationSource =
+  | "mention"
+  | "approval"
+  | "needs_action"
+  | "dm"
+  | "thread_reply";
+
+const MESSAGE_BODY_FALLBACKS: Record<MessageNotificationSource, string> = {
+  mention: "Something in Buzz needs your attention.",
+  approval: "A workflow is waiting for your approval.",
+  needs_action: "Something in Buzz needs your attention.",
+  dm: "New message",
+  thread_reply: "New reply",
+};
+
+/**
+ * Canonical copy for every message-shaped desktop notification (home-feed
+ * mentions and needs-action items, live DMs, live thread replies). All paths
+ * format through here so sender attribution and fallbacks stay consistent:
+ * the sender leads the title whenever their profile has resolved, and each
+ * source degrades to neutral copy — never a raw pubkey — when it has not.
+ *
+ * `senderName` must already be a real human label (see
+ * `senderNameFromSummary`); `channelName` is the raw channel name without
+ * a `#` prefix.
+ */
+export function formatMessageNotification(opts: {
+  source: MessageNotificationSource;
+  senderName?: string | null;
+  channelName?: string | null;
+  content: string;
+}): { title: string; body: string } {
+  const { source, content } = opts;
+  const senderName = opts.senderName?.trim() || null;
+  const channelName = opts.channelName?.trim() || null;
+  const body = truncateNotificationBody(
+    content,
+    MESSAGE_BODY_FALLBACKS[source],
+  );
+
+  if (source === "dm") {
+    return { title: senderName ?? channelName ?? "Direct message", body };
+  }
+
+  const channelLabel = channelName ? `#${channelName}` : null;
+  const prefix =
+    source === "mention"
+      ? senderName
+        ? `${senderName} mentioned you`
+        : "@Mention"
+      : source === "approval"
+        ? senderName
+          ? `${senderName} requested approval`
+          : "Approval Requested"
+        : source === "thread_reply"
+          ? senderName
+            ? `${senderName} replied`
+            : "Reply"
+          : (senderName ?? "Needs Action");
+
+  return { title: formatNotificationTitle({ prefix, channelLabel }), body };
+}

AI-Native-Ventures/Colony

(cherry picked from commit 4e3c9e619c93dd26677b392ad1f8cf0d12c8f855)
Signed-off-by: Basheer Phiri <phiribash@gmail.com>

desktop/src/features/notifications/lib/notificationFormat.test.mjs
@@ -1,0 +1,163 @@
+import assert from "node:assert/strict";
+import test from "node:test";
+
+import { formatMessageNotification } from "./notificationFormat.ts";
+import { senderNameFromSummary } from "./senderName.ts";
+
+test("DM title is the sender name when resolved", () => {
+  const { title, body } = formatMessageNotification({
+    source: "dm",
+    senderName: "Taylor",
+    channelName: "taylor-wes",
+    content: "hey there",
+  });
+
+  assert.equal(title, "Taylor");
+  assert.equal(body, "hey there");
+});
+
+test("DM title falls back to the channel name, then generic copy", () => {
+  assert.equal(
+    formatMessageNotification({
+      source: "dm",
+      senderName: null,
+      channelName: "taylor-wes",
+      content: "hi",
+    }).title,
+    "taylor-wes",
+  );
+  assert.equal(
+    formatMessageNotification({
+      source: "dm",
+      senderName: "  ",
+      channelName: "",
+      content: "hi",
+    }).title,
+    "Direct message",
+  );
+});
+
+test("DM body falls back when the message is blank", () => {
+  assert.equal(
+    formatMessageNotification({
+      source: "dm",
+      senderName: "Taylor",
+      channelName: null,
+      content: "   ",
+    }).body,
+    "New message",
+  );
+});
+
+test("thread reply leads with the sender when resolved", () => {
+  assert.equal(
+    formatMessageNotification({
+      source: "thread_reply",
+      senderName: "Taylor",
+      channelName: "ship-room",
+      content: "done!",
+    }).title,
+    "Taylor replied in #ship-room",
+  );
+});
+
+test("thread reply preserves legacy copy when the sender is unknown", () => {
+  assert.equal(
+    formatMessageNotification({
+      source: "thread_reply",
+      senderName: null,
+      channelName: "ship-room",
+      content: "done!",
+    }).title,
+    "Reply in #ship-room",
+  );
+  assert.deepEqual(
+    formatMessageNotification({
+      source: "thread_reply",
+      senderName: null,
+      channelName: null,
+      content: "",
+    }),
+    { title: "Reply", body: "New reply" },
+  );
+});
+
+test("mention titles match the home-feed conventions", () => {
+  assert.equal(
+    formatMessageNotification({
+      source: "mention",
+      senderName: "Taylor",
+      channelName: "ship-room",
+      content: "@wes look",
+    }).title,
+    "Taylor mentioned you in #ship-room",
+  );
+  assert.equal(
+    formatMessageNotification({
+      source: "mention",
+      senderName: null,
+      channelName: "ship-room",
+      content: "@wes look",
+    }).title,
+    "@Mention in #ship-room",
+  );
+});
+
+test("approval and needs-action titles match the home-feed conventions", () => {
+  assert.deepEqual(
+    formatMessageNotification({
+      source: "approval",
+      senderName: "Taylor",
+      channelName: "ops",
+      content: "",
+    }),
+    {
+      title: "Taylor requested approval in #ops",
+      body: "A workflow is waiting for your approval.",
+    },
+  );
+  assert.deepEqual(
+    formatMessageNotification({
+      source: "needs_action",
+      senderName: null,
+      channelName: null,
+      content: "",
+    }),
+    {
+      title: "Needs Action",
+      body: "Something in Colony needs your attention.",
+    },
+  );
+});
+
+test("senderNameFromSummary prefers displayName, then NIP-05, never a pubkey", () => {
+  assert.equal(
+    senderNameFromSummary({
+      displayName: "Taylor",
+      avatarUrl: null,
+      nip05Handle: "taylor@buzz.example",
+      ownerPubkey: null,
+    }),
+    "Taylor",
+  );
+  assert.equal(
+    senderNameFromSummary({
+      displayName: "  ",
+      avatarUrl: null,
+      nip05Handle: "taylor@buzz.example",
+      ownerPubkey: null,
+    }),
+    "taylor@buzz.example",
+  );
+  assert.equal(
+    senderNameFromSummary({
+      displayName: null,
+      avatarUrl: null,
+      nip05Handle: null,
+      ownerPubkey: null,
+    }),
+    null,
+  );
+  assert.equal(senderNameFromSummary(null), null);
+  assert.equal(senderNameFromSummary(undefined), null);
+});

desktop/src/features/notifications/lib/notificationFormat.ts
@@ -47,3 +47,66 @@ export function formatNotificationTitle(opts: {
     ? `${opts.prefix} in ${opts.channelLabel}`
     : opts.prefix;
 }
+
+export type MessageNotificationSource =
+  | "mention"
+  | "approval"
+  | "needs_action"
+  | "dm"
+  | "thread_reply";
+
+const MESSAGE_BODY_FALLBACKS: Record<MessageNotificationSource, string> = {
+  mention: "Something in Colony needs your attention.",
+  approval: "A workflow is waiting for your approval.",
+  needs_action: "Something in Colony needs your attention.",
+  dm: "New message",
+  thread_reply: "New reply",
+};
+
+/**
+ * Canonical copy for every message-shaped desktop notification (home-feed
+ * mentions and needs-action items, live DMs, live thread replies). All paths
+ * format through here so sender attribution and fallbacks stay consistent:
+ * the sender leads the title whenever their profile has resolved, and each
+ * source degrades to neutral copy — never a raw pubkey — when it has not.
+ *
+ * `senderName` must already be a real human label (see
+ * `senderNameFromSummary`); `channelName` is the raw channel name without
+ * a `#` prefix.
+ */
+export function formatMessageNotification(opts: {
+  source: MessageNotificationSource;
+  senderName?: string | null;
+  channelName?: string | null;
+  content: string;
+}): { title: string; body: string } {
+  const { source, content } = opts;
+  const senderName = opts.senderName?.trim() || null;
+  const channelName = opts.channelName?.trim() || null;
+  const body = truncateNotificationBody(
+    content,
+    MESSAGE_BODY_FALLBACKS[source],
+  );
+
+  if (source === "dm") {
+    return { title: senderName ?? channelName ?? "Direct message", body };
+  }
+
+  const channelLabel = channelName ? `#${channelName}` : null;
+  const prefix =
+    source === "mention"
+      ? senderName
+        ? `${senderName} mentioned you`
+        : "@Mention"
+      : source === "approval"
+        ? senderName
+          ? `${senderName} requested approval`
+          : "Approval Requested"
+        : source === "thread_reply"
+          ? senderName
+            ? `${senderName} replied`
+            : "Reply"
+          : (senderName ?? "Needs Action");
+
+  return { title: formatNotificationTitle({ prefix, channelLabel }), body };
+}