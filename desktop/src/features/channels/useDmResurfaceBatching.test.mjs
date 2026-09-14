5dive-ai/5dive-chat

fix(client): resurface hidden DMs from live activity (#6885)
## Summary

Hiding a DM row and then receiving a new message from that person never
brought the row back — the reader was left with only an Inbox
notification. This restores the row from live activity, client-side,
with no relay or database changes.

- Subscribe to hidden-DM activity **channel-scoped** over the hidden-DM
id set (`#h`), re-registering whenever the set changes. The relay's
`fan_out_scoped` only routes channel-scoped events (every kind:9 DM
carries a `channel_id`) to channel-scoped subscriptions, so a
community-global `#p` filter receives nothing. Hiding a DM never drops
membership, so `#h` authorization holds. Because every delivered event
is already for a hidden DM the reader belongs to, eligibility no longer
needs a self `p` tag — untagged CLI/agent DMs resurface too — and no
per-event visibility fetch is required.
- On qualifying activity, invoke the existing open-DM action (`41010`)
to reopen the hidden one-to-one or group DM, validating that the relay
reopened the same channel.
- Preserve the existing relay-synced hide snapshot (`41012` → `30622`);
hide state still syncs across devices.
- Let Inbox reopen a hidden DM before navigating to its message, with a
busy state on the row and context-menu "Open in channel" action while a
reopen is pending.
- Coalesce resurface attempts per channel: a concurrent message that
arrives while a reopen is in flight triggers a retry on failure instead
of being marked handled and dropped.
- Write exact dm-visibility query keys on open/hide mutations.

This is the client-only replacement agreed in the Buzz design thread and
supersedes #6713. The accepted limitation is that resurface only fires
while a client is running — a message that arrives while all clients are
closed does not restore the row on next launch (the `30622` snapshot has
no watermark); Inbox click-through is the safety net for that case.

Addresses #6316.

---------

Signed-off-by: kenny l

desktop/src/features/channels/useDmResurfaceBatching.test.mjs
@@ -1,0 +1,278 @@
+/**
+ * Batching lifecycle for useDmResurfaceFromMessages.
+ *
+ * The relay rejects a REQ whose aggregate explicit `#h` values exceed
+ * MAX_EXPLICIT_CHANNEL_VALUES, so a hidden set larger than the cap must be
+ * split into multiple subscriptions or every hidden DM loses its resurface
+ * trigger. These tests exercise exactly that split and its teardown, which a
+ * pure helper test cannot reach because the batching only happens inside the
+ * mounted effect.
+ */
+
+import assert from "node:assert/strict";
+import { after, before, test } from "node:test";
+
+import { JSDOM } from "jsdom";
+
+const dom = new JSDOM("<!doctype html><html><body></body></html>", {
+  url: "http://localhost",
+});
+
+before(() => {
+  Object.assign(globalThis, {
+    document: dom.window.document,
+    HTMLElement: dom.window.HTMLElement,
+    IS_REACT_ACT_ENVIRONMENT: true,
+    window: dom.window,
+    localStorage: dom.window.localStorage,
+  });
+  globalThis.__TAURI_INTERNALS__ = {
+    invoke: (cmd) => {
+      if (cmd === "get_channel_members") {
+        return Promise.resolve({
+          members: [
+            { pubkey: VIEWER, role: "member", is_agent: false },
+            { pubkey: "b".repeat(64), role: "member", is_agent: false },
+          ],
+        });
+      }
+      return Promise.reject(new Error(`unmocked Tauri command: ${cmd}`));
+    },
+    transformCallback: () => Math.random(),
+  };
+  dom.window.__TAURI_INTERNALS__ = globalThis.__TAURI_INTERNALS__;
+});
+
+after(() => dom.window.close());
+
+const RELAY_URL = "wss://relay.example";
+const VIEWER = "a".repeat(64);
+
+function seedCommunity() {
+  window.localStorage.setItem(
+    "buzz-communities",
+    JSON.stringify([
+      {
+        id: "community-a",
+        name: "Community A",
+        relayUrl: RELAY_URL,
+        addedAt: "2026-01-01T00:00:00Z",
+      },
+    ]),
+  );
+  window.localStorage.setItem("buzz-active-community-id", "community-a");
+}
+
+function hiddenDmSnapshot(count) {
+  return {
+    id: "snapshot-1",
+    kind: 30622,
+    pubkey: VIEWER,
+    content: "",
+    created_at: 1,
+    tags: Array.from({ length: count }, (_, index) => [
+      "h",
+      `dm-${String(index).padStart(4, "0")}`,
+    ]),
+    sig: "",
+  };
+}
+
+async function mount(hiddenCount, subscribeImpl, reopen) {
+  const { act, cleanup, renderHook } = await import("@testing-library/react");
+  const React = (await import("react")).default;
+  const { QueryClient, QueryClientProvider } = await import(
+    "@tanstack/react-query"
+  );
+  const { CommunitiesProvider } = await import(
+    "@/features/communities/useCommunities.tsx"
+  );
+  const { relayClient } = await import("@/shared/api/relayClient");
+  const { useDmResurfaceFromMessages } = await import(
+    "./useDmResurfaceFromMessages.ts"
+  );
+
+  const originalFetchEvents = relayClient.fetchEvents;
+  const originalSubscribeLive = relayClient.subscribeLive;
+
+  relayClient.fetchEvents = async () =>
+    hiddenCount > 0 ? [hiddenDmSnapshot(hiddenCount)] : [];
+  relayClient.subscribeLive = subscribeImpl;
+
+  const queryClient = new QueryClient({
+    defaultOptions: { queries: { retry: false, gcTime: 0 } },
+  });
+
+  const wrapper = ({ children }) =>
+    React.createElement(
+      QueryClientProvider,
+      { client: queryClient },
+      React.createElement(CommunitiesProvider, null, children),
+    );
+
+  const hook = renderHook(
+    () =>
+      useDmResurfaceFromMessages({
+        pubkey: VIEWER,
+        relayUrl: RELAY_URL,
+        reopen: reopen ?? (async () => ({ id: "x" })),
+      }),
+    { wrapper },
+  );
+
+  return {
+    act,
+    async settle() {
+      for (let i = 0; i < 6; i++) {
+        await act(async () => {
+          await new Promise((r) => setTimeout(r, 5));
+        });
+      }
+    },
+    unmount: hook.unmount,
+    restore() {
+      hook.unmount();
+      queryClient.clear();
+      queryClient.unmount();
+      cleanup();
+      relayClient.fetchEvents = originalFetchEvents;
+      relayClient.subscribeLive = originalSubscribeLive;
+    },
+  };
+}
+
+test("128 hidden DMs use a single subscription within the cap", async () => {
+  seedCommunity();
+  const batches = [];
+  const harness = await mount(128, async (filter) => {
+    batches.push(filter["#h"]);
+    return async () => {};
+  });
+  try {
+    await harness.settle();
+    assert.equal(batches.length, 1);
+    assert.equal(batches[0].length, 128);
+  } finally {
+    harness.restore();
+  }
+});
+
+test("129 hidden DMs split into two subscriptions, both within the cap", async () => {
+  seedCommunity();
+  const batches = [];
+  const harness = await mount(129, async (filter) => {
+    batches.push(filter["#h"]);
+    return async () => {};
+  });
+  try {
+    await harness.settle();
+    assert.equal(batches.length, 2);
+    assert.deepEqual(
+      batches.map((batch) => batch.length),
+      [128, 1],
+    );
+    // Every hidden id appears exactly once across the batches.
+    const all = batches.flat();
+    assert.equal(new Set(all).size, 129);
+    for (const batch of batches) {
+      assert.ok(batch.length <= 128);
+    }
+  } finally {
+    harness.restore();
+  }
+});
+
+test("activity delivered on the final batch resurfaces its DM", async () => {
+  seedCommunity();
+  const handlers = [];
+  const reopened = [];
+  const targetIdRef = { id: null };
+  const harness = await mount(
+    129,
+    async (filter, onEvent) => {
+      handlers.push({ ids: filter["#h"], onEvent });
+      return async () => {};
+    },
+    async ({ pubkeys }) => {
+      reopened.push(pubkeys);
+      // The reopen contract returns the resurfaced channel id; the action
+      // rejects a mismatch, so echo the target the event carried.
+      return { id: targetIdRef.id };
+    },
+  );
+  try {
+    await harness.settle();
+    const finalBatch = handlers[1];
+    const targetId = finalBatch.ids[0];
+    targetIdRef.id = targetId;
+    // Deliver a peer message on the FINAL batch's subscription. If batching
+    // dropped that batch's handler, this event would never reach the
+    // coordinator and reopen would never fire.
+    await harness.act(async () => {
+      finalBatch.onEvent({
+        id: "evt-1",
+        kind: 9,
+        pubkey: "b".repeat(64),
+        content: "hi",
+        created_at: 2,
+        tags: [["h", targetId]],
+        sig: "",
+      });
+      await new Promise((r) => setTimeout(r, 5));
+    });
+    assert.equal(reopened.length, 1);
+    assert.deepEqual(reopened[0], ["b".repeat(64)]);
+  } finally {
+    harness.restore();
+  }
+});
+
+test("a batch subscription failure does not abort the other batches", async () => {
+  seedCommunity();
+  const batches = [];
+  let call = 0;
+  const harness = await mount(129, async (filter) => {
+    call += 1;
+    if (call === 1) throw new Error("relay rejected batch 1");
+    batches.push(filter["#h"]);
+    return async () => {};
+  });
+  try {
+    await harness.settle();
+    // The second batch still subscribed even though the first threw.
+    assert.equal(batches.length, 1);
+    assert.equal(batches[0].length, 1);
+  } finally {
+    harness.restore();
+  }
+});
+
+test("teardown while batch setup is pending disposes every settled batch", async () => {
+  seedCommunity();
+  let disposeCount = 0;
+  const releases = [];
+  const harness = await mount(129, async () => {
+    // Each subscribe parks until the test releases it, so the effect can be
+    // torn down while batch setup is still in flight.
+    await new Promise((resolve) => releases.push(resolve));
+    return async () => {
+      disposeCount += 1;
+    };
+  });
+  try {
+    await harness.settle();
+    // Unmount before any subscribe resolves.
+    await harness.act(async () => {
+      harness.unmount();
+    });
+    // Now let both subscribes resolve; each must be disposed since its owning
+    // generation is gone.
+    await harness.act(async () => {
+      for (const release of releases) release();
+      await new Promise((r) => setTimeout(r, 5));
+    });
+    assert.equal(disposeCount, 2);
+  } finally {
+    harness.restore();
+  }
+});

desktop/src/features/home/hiddenDmInboxAction.ts
@@ -1,0 +1,76 @@
+import { dmPeerPubkeysFromMembers } from "@/features/channels/dmResurface";
+import type { InboxItem } from "@/features/home/lib/inbox";
+import type { ChannelMember } from "@/shared/api/types";
+import type { OpenDmInput } from "@/shared/api/tauriChannels";
+
+type HiddenDmInboxActionOptions = {
+  item: InboxItem;
+  channelId: string;
+  messageId: string;
+  threadRootId?: string | null;
+  availableChannelIds: ReadonlySet<string>;
+  expectedRelayUrl: string;
+  expectedSignerPubkey: string;
+  pendingChannelIds: Set<string>;
+  fetchMembers: (channelId: string) => Promise<readonly ChannelMember[]>;
+  openDm: (input: OpenDmInput) => Promise<{ id: string }>;
+  isCurrent: () => boolean;
+  onOpenContext: (
+    channelId: string,
+    messageId: string,
+    threadRootId?: string | null,
+  ) => void;
+  onError: () => void;
+  onPendingChange: () => void;
+};
+
+export async function openHiddenDmInboxContext({
+  item,
+  channelId,
+  messageId,
+  threadRootId,
+  availableChannelIds,
+  expectedRelayUrl,
+  expectedSignerPubkey,
+  pendingChannelIds,
+  fetchMembers,
+  openDm,
+  isCurrent,
+  onOpenContext,
+  onError,
+  onPendingChange,
+}: HiddenDmInboxActionOptions): Promise<boolean> {
+  if (availableChannelIds.has(channelId) || item.item.channelType !== "dm") {
+    if (isCurrent()) onOpenContext(channelId, messageId, threadRootId);
+    return true;
+  }
+  if (pendingChannelIds.has(channelId)) return false;
+
+  pendingChannelIds.add(channelId);
+  onPendingChange();
+  try {
+    const members = await fetchMembers(channelId);
+    if (!isCurrent()) return false;
+    const pubkeys = dmPeerPubkeysFromMembers(members, expectedSignerPubkey);
+    if (pubkeys.length === 0) {
+      throw new Error("Could not determine the DM membership.");
+    }
+    const opened = await openDm({
+      pubkeys,
+      expectedRelayUrl,
+      expectedSignerPubkey,
+    });
+    if (!isCurrent()) return false;
+    if (opened.id !== channelId) {
+      throw new Error("Relay reopened a different DM conversation.");
+    }
+    onOpenContext(channelId, messageId, threadRootId);
+    return true;
+  } catch {
+    if (isCurrent()) onError();
+    return false;
+  } finally {
+    pendingChannelIds.delete(channelId);
+    if (isCurrent()) onPendingChange();
+  }
+}

AI-Native-Ventures/Colony

(cherry picked from commit 4d77175c78f4df48e13398b25b271e3ac1b31dd3)
Signed-off-by: Basheer Phiri <phiribash@gmail.com>

desktop/src/features/channels/useDmResurfaceBatching.test.mjs
@@ -1,0 +1,285 @@
+/**
+ * Batching lifecycle for useDmResurfaceFromMessages.
+ *
+ * The relay rejects a REQ whose aggregate explicit `#h` values exceed
+ * MAX_EXPLICIT_CHANNEL_VALUES, so a hidden set larger than the cap must be
+ * split into multiple subscriptions or every hidden DM loses its resurface
+ * trigger. These tests exercise exactly that split and its teardown, which a
+ * pure helper test cannot reach because the batching only happens inside the
+ * mounted effect.
+ */
+
+import assert from "node:assert/strict";
+import { after, before, test } from "node:test";
+
+import { JSDOM } from "jsdom";
+
+const dom = new JSDOM("<!doctype html><html><body></body></html>", {
+  url: "http://localhost",
+});
+
+before(() => {
+  Object.assign(globalThis, {
+    document: dom.window.document,
+    HTMLElement: dom.window.HTMLElement,
+    IS_REACT_ACT_ENVIRONMENT: true,
+    window: dom.window,
+    localStorage: dom.window.localStorage,
+  });
+});
+
+// Colony routes every native call through the NativeBridge seam, so the
+// upstream `__TAURI_INTERNALS__` stub would never be consulted.
+before(async () => {
+  const { setNativeBridge } = await import("@/shared/api/nativeBridge");
+  const { createMockNativeBridge } = await import(
+    "@/testing/createMockNativeBridge"
+  );
+  setNativeBridge(
+    createMockNativeBridge((command) => {
+      if (command === "get_channel_members") {
+        return {
+          members: [
+            { pubkey: VIEWER, role: "member", is_agent: false },
+            { pubkey: "b".repeat(64), role: "member", is_agent: false },
+          ],
+        };
+      }
+      throw new Error(`unmocked native command: ${command}`);
+    }),
+  );
+});
+
+after(() => dom.window.close());
+
+const RELAY_URL = "wss://relay.example";
+const VIEWER = "a".repeat(64);
+
+function seedCommunity() {
+  window.localStorage.setItem(
+    "buzz-communities",
+    JSON.stringify([
+      {
+        id: "community-a",
+        name: "Community A",
+        relayUrl: RELAY_URL,
+        addedAt: "2026-01-01T00:00:00Z",
+      },
+    ]),
+  );
+  window.localStorage.setItem("buzz-active-community-id", "community-a");
+}
+
+function hiddenDmSnapshot(count) {
+  return {
+    id: "snapshot-1",
+    kind: 30622,
+    pubkey: VIEWER,
+    content: "",
+    created_at: 1,
+    tags: Array.from({ length: count }, (_, index) => [
+      "h",
+      `dm-${String(index).padStart(4, "0")}`,
+    ]),
+    sig: "",
+  };
+}
+
+async function mount(hiddenCount, subscribeImpl, reopen) {
+  const { act, cleanup, renderHook } = await import("@testing-library/react");
+  const React = (await import("react")).default;
+  const { QueryClient, QueryClientProvider } = await import(
+    "@tanstack/react-query"
+  );
+  const { CommunitiesProvider } = await import(
+    "@/features/communities/useCommunities.tsx"
+  );
+  const { relayClient } = await import("@/shared/api/relayClient");
+  const { useDmResurfaceFromMessages } = await import(
+    "./useDmResurfaceFromMessages.ts"
+  );
+
+  const originalFetchEvents = relayClient.fetchEvents;
+  const originalSubscribeLive = relayClient.subscribeLive;
+
+  relayClient.fetchEvents = async () =>
+    hiddenCount > 0 ? [hiddenDmSnapshot(hiddenCount)] : [];
+  relayClient.subscribeLive = subscribeImpl;
+
+  const queryClient = new QueryClient({
+    defaultOptions: { queries: { retry: false, gcTime: 0 } },
+  });
+
+  const wrapper = ({ children }) =>
+    React.createElement(
+      QueryClientProvider,
+      { client: queryClient },
+      React.createElement(CommunitiesProvider, null, children),
+    );
+
+  const hook = renderHook(
+    () =>
+      useDmResurfaceFromMessages({
+        pubkey: VIEWER,
+        relayUrl: RELAY_URL,
+        reopen: reopen ?? (async () => ({ id: "x" })),
+      }),
+    { wrapper },
+  );
+
+  return {
+    act,
+    async settle() {
+      for (let i = 0; i < 6; i++) {
+        await act(async () => {
+          await new Promise((r) => setTimeout(r, 5));
+        });
+      }
+    },
+    unmount: hook.unmount,
+    restore() {
+      hook.unmount();
+      queryClient.clear();
+      queryClient.unmount();
+      cleanup();
+      relayClient.fetchEvents = originalFetchEvents;
+      relayClient.subscribeLive = originalSubscribeLive;
+    },
+  };
+}
+
+test("128 hidden DMs use a single subscription within the cap", async () => {
+  seedCommunity();
+  const batches = [];
+  const harness = await mount(128, async (filter) => {
+    batches.push(filter["#h"]);
+    return async () => {};
+  });
+  try {
+    await harness.settle();
+    assert.equal(batches.length, 1);
+    assert.equal(batches[0].length, 128);
+  } finally {
+    harness.restore();
+  }
+});
+
+test("129 hidden DMs split into two subscriptions, both within the cap", async () => {
+  seedCommunity();
+  const batches = [];
+  const harness = await mount(129, async (filter) => {
+    batches.push(filter["#h"]);
+    return async () => {};
+  });
+  try {
+    await harness.settle();
+    assert.equal(batches.length, 2);
+    assert.deepEqual(
+      batches.map((batch) => batch.length),
+      [128, 1],
+    );
+    // Every hidden id appears exactly once across the batches.
+    const all = batches.flat();
+    assert.equal(new Set(all).size, 129);
+    for (const batch of batches) {
+      assert.ok(batch.length <= 128);
+    }
+  } finally {
+    harness.restore();
+  }
+});
+
+test("activity delivered on the final batch resurfaces its DM", async () => {
+  seedCommunity();
+  const handlers = [];
+  const reopened = [];
+  const targetIdRef = { id: null };
+  const harness = await mount(
+    129,
+    async (filter, onEvent) => {
+      handlers.push({ ids: filter["#h"], onEvent });
+      return async () => {};
+    },
+    async ({ pubkeys }) => {
+      reopened.push(pubkeys);
+      // The reopen contract returns the resurfaced channel id; the action
+      // rejects a mismatch, so echo the target the event carried.
+      return { id: targetIdRef.id };
+    },
+  );
+  try {
+    await harness.settle();
+    const finalBatch = handlers[1];
+    const targetId = finalBatch.ids[0];
+    targetIdRef.id = targetId;
+    // Deliver a peer message on the FINAL batch's subscription. If batching
+    // dropped that batch's handler, this event would never reach the
+    // coordinator and reopen would never fire.
+    await harness.act(async () => {
+      finalBatch.onEvent({
+        id: "evt-1",
+        kind: 9,
+        pubkey: "b".repeat(64),
+        content: "hi",
+        created_at: 2,
+        tags: [["h", targetId]],
+        sig: "",
+      });
+      await new Promise((r) => setTimeout(r, 5));
+    });
+    assert.equal(reopened.length, 1);
+    assert.deepEqual(reopened[0], ["b".repeat(64)]);
+  } finally {
+    harness.restore();
+  }
+});
+
+test("a batch subscription failure does not abort the other batches", async () => {
+  seedCommunity();
+  const batches = [];
+  let call = 0;
+  const harness = await mount(129, async (filter) => {
+    call += 1;
+    if (call === 1) throw new Error("relay rejected batch 1");
+    batches.push(filter["#h"]);
+    return async () => {};
+  });
+  try {
+    await harness.settle();
+    // The second batch still subscribed even though the first threw.
+    assert.equal(batches.length, 1);
+    assert.equal(batches[0].length, 1);
+  } finally {
+    harness.restore();
+  }
+});
+
+test("teardown while batch setup is pending disposes every settled batch", async () => {
+  seedCommunity();
+  let disposeCount = 0;
+  const releases = [];
+  const harness = await mount(129, async () => {
+    // Each subscribe parks until the test releases it, so the effect can be
+    // torn down while batch setup is still in flight.
+    await new Promise((resolve) => releases.push(resolve));
+    return async () => {
+      disposeCount += 1;
+    };
+  });
+  try {
+    await harness.settle();
+    // Unmount before any subscribe resolves.
+    await harness.act(async () => {
+      harness.unmount();
+    });
+    // Now let both subscribes resolve; each must be disposed since its owning
+    // generation is gone.
+    await harness.act(async () => {
+      for (const release of releases) release();
+      await new Promise((r) => setTimeout(r, 5));
+    });
+    assert.equal(disposeCount, 2);
+  } finally {
+    harness.restore();
+  }
+});

desktop/src/features/home/hiddenDmInboxAction.ts
@@ -1,0 +1,72 @@
+import { dmPeerPubkeysFromMembers } from "@/features/channels/dmResurface";
+import type { InboxItem } from "@/features/home/lib/inbox";
+import type { ChannelMember } from "@/shared/api/types";
+import type { OpenDmInput } from "@/shared/api/types";
+
+type HiddenDmInboxActionOptions = {
+  item: InboxItem;
+  channelId: string;
+  messageId: string;
+  threadRootId?: string | null;
+  availableChannelIds: ReadonlySet<string>;
+  expectedSignerPubkey: string;
+  pendingChannelIds: Set<string>;
+  fetchMembers: (channelId: string) => Promise<readonly ChannelMember[]>;
+  openDm: (input: OpenDmInput) => Promise<{ id: string }>;
+  isCurrent: () => boolean;
+  onOpenContext: (
+    channelId: string,
+    messageId: string,
+    threadRootId?: string | null,
+  ) => void;
+  onError: () => void;
+  onPendingChange: () => void;
+};
+
+export async function openHiddenDmInboxContext({
+  item,
+  channelId,
+  messageId,
+  threadRootId,
+  availableChannelIds,
+  expectedSignerPubkey,
+  pendingChannelIds,
+  fetchMembers,
+  openDm,
+  isCurrent,
+  onOpenContext,
+  onError,
+  onPendingChange,
+}: HiddenDmInboxActionOptions): Promise<boolean> {
+  if (availableChannelIds.has(channelId) || item.item.channelType !== "dm") {
+    if (isCurrent()) onOpenContext(channelId, messageId, threadRootId);
+    return true;
+  }
+  if (pendingChannelIds.has(channelId)) return false;
+
+  pendingChannelIds.add(channelId);
+  onPendingChange();
+  try {
+    const members = await fetchMembers(channelId);
+    if (!isCurrent()) return false;
+    const pubkeys = dmPeerPubkeysFromMembers(members, expectedSignerPubkey);
+    if (pubkeys.length === 0) {
+      throw new Error("Could not determine the DM membership.");
+    }
+    // Colony's open_dm takes only `pubkeys`; upstream's tenant fence on the
+    // command is not ported, so the scope guard stays on this side.
+    const opened = await openDm({ pubkeys });
+    if (!isCurrent()) return false;
+    if (opened.id !== channelId) {
+      throw new Error("Relay reopened a different DM conversation.");
+    }
+    onOpenContext(channelId, messageId, threadRootId);
+    return true;
+  } catch {
+    if (isCurrent()) onError();
+    return false;
+  } finally {
+    pendingChannelIds.delete(channelId);
+    if (isCurrent()) onPendingChange();
+  }
+}